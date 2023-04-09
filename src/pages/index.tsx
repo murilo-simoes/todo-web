085d2793ef15360ce916199a89dea0860655359d
murilo-simoes <murilorsimoes@gmail.com>
2023-04-09 15:48:58 -0300

    lista

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,31 @@ export default function Home() {
   return (
     <>
       <ToastContainer theme="dark" />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "80%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "2rem",
+            color: "#eee",
+            fontFamily: "Poppins",
+          }}
+        >
+          <h1>
+            No momento, o servidor gratuito que utilizo para o banco de dados
+            funcionar atingiu o limite de requisições gratuitas, então
+            infelizmente a lista não está funcionando.
+          </h1>
+        </div>
+      </div>
       <TaskList />
       <CreateTask />
     </>
